281f832b3fd1b9cbfdc30b4060ff75f317d9ea3d
Rodrigo Bossini <professorbossini@gmail.com>
2021-02-12 21:23:12 -0300

    Mais duas funcionalidades nos comentarios

--- EntregaGit.java
+++ EntregaGit.java
@@ -3,5 +3,7 @@ public class EntregaGit{
   public static void main(String[] args) {
     String nome = JOptionPane.showInputDialog("Qual o seu nome?");
     JOptionPane.showMessageDialog(null, String.format("Bem vindo, %s", nome));
+    //outra funcionalidade
+    //mais uma
   }
 }
